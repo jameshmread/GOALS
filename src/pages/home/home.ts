378af0ba22f35008bbe40b1f096a2103327c379f
jameshmread <jamesread117@yahoo.co.uk>
2018-05-17 17:10:00 +0100

    Goals can now be 'swiped' to reveal more options

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,53 +1,58 @@
-import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
-import { AddGoalPage } from '../add-goal/add-goal';
-import { GoalDetailsPage } from '../goal-details/goal-details';
-import { GoalStoreProvider } from '../../providers/goal-store/goal-store';
- 
+import { Component } from "@angular/core";
+import { NavController, ModalController } from "ionic-angular";
+import { AddGoalPage } from "../add-goal/add-goal";
+import { GoalDetailsPage } from "../goal-details/goal-details";
+import { GoalStoreProvider } from "../../providers/goal-store/goal-store";
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
- 
+
   public items = [];
- 
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public goalService: GoalStoreProvider) {
+
+  constructor (
+    public navCtrl: NavController,
+    public modalCtrl: ModalController, public goalService: GoalStoreProvider) {
     this.goalService.getGoals().then((todos) => {
- 
-      if(todos){
+
+      if (todos){
         this.items = todos;
       }
- 
+
     });
   }
- 
-  ionViewDidLoad(){
- 
+
+  public ionViewDidLoad (){
+
   }
- 
-  addItem(){
- 
-    let addModal = this.modalCtrl.create(AddGoalPage);
- 
+
+  public addItem (){
+
+    const addModal = this.modalCtrl.create(AddGoalPage);
+
     addModal.onDidDismiss((item) => {
-          if(item){
+          if (item){
             this.saveItem(item);
           }
     });
- 
+
     addModal.present();
- 
+
   }
- 
-  saveItem(item){
+
+  public saveItem (item){
     this.items.push(item);
     this.goalService.save(this.items);
   }
- 
-  viewItem(item){
-    this.navCtrl.push(GoalDetailsPage, {
-      item: item
-    });
+
+  public viewItem (item){
+    this.navCtrl.push(GoalDetailsPage, {item});
   }
+
+  public incrementCompletion () {
+    throw Error ("Not implemented yet");
+  }
+
 }
