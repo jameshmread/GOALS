f82102553ab3d91215dbf10e0d0451037537b660
jameshmread <jamesread117@yahoo.co.uk>
2018-05-24 15:46:27 +0100

    goals now have a type of completion

--- src/DTOs/Goal.ts
+++ src/DTOs/Goal.ts
@@ -1,8 +1,10 @@
 export class Goal {
+    public currentCompletion = 0;
     constructor (
         public title: string,
         public description: string,
         public dateTime: string,
-        public type: string
+        public type: string,
+        public maxCompletion: number
     ) {}
 }
