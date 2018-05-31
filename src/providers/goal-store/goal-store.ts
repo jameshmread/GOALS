7ac38fcd8bfbb530a3bfeb7f6618e81e78a7e948
jameshmread <jamesread117@yahoo.co.uk>
2018-05-31 12:39:36 +0100

    returning no daily goals no longer throws error

--- src/providers/goal-store/goal-store.ts
+++ src/providers/goal-store/goal-store.ts
@@ -22,10 +22,13 @@ export class GoalStoreProvider {
 
   public getTodaysGoals (): Promise<Array<Goal>> {
     return this.getGoals().then((goals) => {
-    return goals.filter((goal) => {
-        return goal.dateTime.getDate() === new Date().getDate();
+      if (!goals) {
+        return [];
+      }
+      return goals.filter((goal) => {
+          return goal.dateTime.getDate() === new Date().getDate();
+        });
       });
-    });
   }
 
   public save (goals: Array<Goal>){
@@ -42,19 +45,16 @@ export class GoalStoreProvider {
 
   public createFakeDays () {
     const day1 = new Day();
-    day1.setCompletionState(DayCompletionState.allComplete);
     day1.incrementGoalsCreated();
     day1.incrementGoalsCompleted();
     day1.setDate(new Date(2018, 0, 1));
     day1.setGoaltitles(["Title1", "Title2"]);
     const day2 = new Day();
-    day2.setCompletionState(DayCompletionState.noneStarted);
     day2.incrementGoalsCreated();
     day2.incrementGoalsCreated();
     day2.setDate(new Date(2018, 0, 2));
     day2.setGoaltitles(["TitleA", "TitleN"]);
     const day3 = new Day();
-    day3.setCompletionState(DayCompletionState.partiallyComplete);
     day3.incrementGoalsCreated();
     day3.incrementGoalsInProgress();
     day3.setDate(new Date(2018, 6, 5));
