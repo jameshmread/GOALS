ceb4490a1e8c680ec0fe7e4ec760592075f97750
jameshmread <jamesread117@yahoo.co.uk>
2018-06-03 18:30:15 +0100

    goal store now removes any undefined entries (suspected bug in ionic)

--- src/providers/goal-store/goal-store.ts
+++ src/providers/goal-store/goal-store.ts
@@ -1,65 +1,52 @@
 import { Storage } from "@ionic/storage";
 import { Injectable } from "@angular/core";
 import { Day } from "../../DTOs/Day";
-import { IDay } from "../../interfaces/IDay";
 import { DayCompletionState } from "../../enums/DayCompletionState";
 import { Goal } from "../../DTOs/Goal";
 
 @Injectable()
 export class GoalStoreProvider {
 
-  public days: Array<IDay> = [];
+  public days: Array<Day> = [];
 
   constructor (public storage: Storage){
     this.storage.get("days").then((daysFromStorage) => {
       if (daysFromStorage !== null) {
+        console.log("Days retrieved from storage");
         this.days = daysFromStorage;
       }
     });
     document.addEventListener("pause", this.saveAllToStorageOnExit, false);
   }
 
-  public saveNewDay (dayToSave: IDay) {
-      if (this.dayExists(dayToSave)) {
-        this.editExistingDay(dayToSave);
-      } else {
-        this.setNewDay(dayToSave);
-      }
+  public saveNewDay (dayToSave: Day) {
+    if (this.dayExists(dayToSave)) {
+      this.editExistingDay(dayToSave);
+    } else {
+      this.setNewDay(dayToSave);
+    }
   }
 
   public getDays () {
     return this.days;
   }
 
-  public createRandomDays () {
-    let startDate = new Date(new Date().getFullYear(), 0, 0);
-    for (let i = 0; i < 300; i++) {
-      const day = new Day();
-      day.date = new Date(startDate.setDate(startDate.getDate() + 1));
-      const goal = new Goal("NAME", "Description...", startDate, "", 1);
-      goal.currentCompletion = 1;
-      goal.maxCompletion = 1;
-      day.goals = [goal, goal, goal];
-      this.days.push(day);
-    }
-    console.log(this.days);
-  }
-
   public saveAllToStorageOnExit () {
     this.storage.set("days", this.days);
   }
 
-  private dayExists (newDay: IDay): boolean {
-    if (this.days.length > 0) { return false; }
+  private dayExists (newDay: Day): boolean {
+    if (this.days.length === 0 || this.days === void 0) { return false; }
+    this.days = this.days.filter((day) => day !== void 0);
     return this.days.filter((day) => day.date.getTime() === newDay.date.getTime()).length !== 0;
   }
 
-  private editExistingDay (dayToEdit: IDay) {
+  private editExistingDay (dayToEdit: Day) {
     this.days.pop();
     this.days.push(dayToEdit);
   }
 
-  private setNewDay (newDay: IDay) {
+  private setNewDay (newDay: Day) {
     this.days.push(newDay);
   }
 }
