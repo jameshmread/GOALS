7cf6935f1aaf1007daed1f29215958208f331a27
jameshmread <jamesread117@yahoo.co.uk>
2018-06-03 18:29:38 +0100

    calendar now uses correct lifecycle hook to update day information

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -4,6 +4,7 @@ import { Day } from "../../DTOs/Day";
 import { DayCompletionState } from "../../enums/DayCompletionState";
 import { DaySummaryPage } from "../day-summary/day-summary";
 import { GoalStoreProvider } from "../../providers/goal-store/goal-store";
+import { ScreenOrientation } from "@ionic-native/screen-orientation";
 
 @Component({
   selector: "page-calendar",
@@ -13,20 +14,25 @@ export class CalendarPage {
 
   public currentYear = new Date().getFullYear();
   public weekDays = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
-  public months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Nov", "Dec"];
+  public months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Nov", "Dec"];
   public days: Array<Array<Day>>;
 
   constructor (
     public navCtrl: NavController,
     public navParams: NavParams,
-    public store: GoalStoreProvider
+    public store: GoalStoreProvider,
+    public screen: ScreenOrientation
   ) {
     this.createEmptyYear();
   }
 
   public ionViewDidLoad () {
     console.log("ionViewDidLoad CalendarPage");
+  }
+
+  public ionViewWillEnter () {
     this.setDayInformation();
+    // this.screen.lock("landscape");
   }
 
   public openDaySummary (day: Day) {
@@ -40,29 +46,27 @@ export class CalendarPage {
       this.days[i] = new Array(7);
       for (let j = 0; j < 7; j++) {
         this.days[i][j] = new Day();
-        this.days[i][j].setDate(startDate);
+        this.days[i][j].date = startDate;
         startDate = new Date(startDate.setDate(startDate.getDate() + 1));
       }
     }
   }
 
   private setDayInformation () {
-    // this.store.saveDays(this.store.createFakeDays());
-    this.store.getDays().then((allDays: Array<Day>) => {
-      console.log(allDays);
-      let savedDaysIndex = 0;
-      if (allDays === null) { return; }
-      for (let i = 0; i < 52; i++) {
-        for (let j = 0; j < 7; j++) {
-          if (allDays[savedDaysIndex] !== void 0) {
-            if (new Date(this.days[i][j].getDate()).getTime() ===
-              new Date (allDays[savedDaysIndex].date).getTime()){
-              this.days[i][j] = allDays[savedDaysIndex];
-              savedDaysIndex++;
-            }
+    const allDays = this.store.getDays();
+    console.log(allDays);
+    let savedDaysIndex = 0;
+    for (let i = 0; i < 52; i++) {
+      for (let j = 0; j < 7; j++) {
+        if (allDays[savedDaysIndex] !== void 0) {
+          if (new Date(this.days[i][j].date).getTime() ===
+            new Date(allDays[savedDaysIndex].date).getTime()) {
+            this.days[i][j] = allDays[savedDaysIndex];
+            this.days[i][j].setCompletionState();
+            savedDaysIndex++;
           }
         }
       }
-    });
+    }
   }
 }
