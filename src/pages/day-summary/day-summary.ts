a37d3826131e117e505cae6d1f4791f3e14d7d5b
jameshmread <jamesread117@yahoo.co.uk>
2018-06-03 18:31:05 +0100

    day summary now shows more human readable date format

--- src/pages/day-summary/day-summary.ts
+++ src/pages/day-summary/day-summary.ts
@@ -11,13 +11,15 @@ export class DaySummaryPage {
 
   public completionState: DayCompletionState;
   public goals: Array<Goal> = [];
-  public date: Date;
+  public date: any;
   public goalComplete: Array<boolean>;
 
   constructor (public navCtrl: NavController, public navParams: NavParams) {
     this.completionState = this.navParams.get("day").completionState;
     this.goals = this.navParams.get("day").goals;
     this.date = this.navParams.get("day").date;
+    this.date = this.date.getDate() + "/"
+      + (this.date.getMonth() + 1) + "/" + this.date.getFullYear();
     this.goalComplete = this.goals.map((goal) => {
       return (goal.maxCompletion - goal.currentCompletion) === 0;
     });
